3ff489d75eb6b2a7a02004665f9bbc9b64dff421
Patrick Demers <patrickdemers6@gmail.com>
2022-11-15 09:18:09 -0600

    when all stops or favorites, do not show vehicles

--- components/Map/components/Vehicles.js
+++ components/Map/components/Vehicles.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { ToastAndroid } from 'react-native';
 import { useRecoilValue } from 'recoil';
 
-import { vehicleLocationState } from '../../../state/atoms';
+import { currentRouteRowState, vehicleLocationState } from '../../../state/atoms';
 import VehicleView from './VehicleView';
 
 const Vehicles = () => {
   const vehicles = useRecoilValue(vehicleLocationState);
+  const currentRouteID = useRecoilValue(currentRouteRowState);
 
   React.useEffect(() => {
     if (vehicles === null || vehicles.length > 0) return;
     ToastAndroid.show('No busses found on route.', ToastAndroid.SHORT);
   }, [vehicles]);
+
+  if (currentRouteID < 0) return null;
 
   return vehicles?.map((v) => <VehicleView key={v.ID} details={v} />);
 };
