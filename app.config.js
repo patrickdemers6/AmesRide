91d4453c0f53fdc41b2d58883e026ea1d67895d8
Patrick Demers <patrickdemers6@gmail.com>
2023-01-18 13:34:47 -0600

    bump version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'Ames Ride',
     slug: 'AmesRide',
-    version: '1.0.7',
+    version: '1.1.0',
     sdkVersion: '47.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
